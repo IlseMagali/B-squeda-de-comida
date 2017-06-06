8d747cf94708693c0a9caef87d29156d43e20f5d
IlseMagali <ilsemke@gmail.com>
2017-06-06 11:03:09 +0000

    filtro y visualización de mapas funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
-// window.addEventListener("load", cargarPagina);
 function cargarPagina() {
-
+  mostrarRestaurantes(restaurantes);
   $("#buscar").submit(filtrarRestaurante);
   $(".card-panel").click(mostrarMapaComida);
 
@@ -14,7 +13,7 @@ function cargarPagina() {
   verificarCoords();
 }
 
-// Geolocalizándome
+// Geolocalización
 function obtenerCoordenadas(posicion) {
   var coordenadas = {
 		lat: posicion.coords.latitude,
@@ -36,12 +35,13 @@ function initMap(coordenadas) {
   });
 }
 
+// Arreglo de restaurantes o lugares de comida
 var restaurantes = [
   {
     nombre: "El Cardenal",
     direccion: "Av. de la Paz Núm. 32, Alvaro Obregon, San Ángel, 01000 Ciudad de México, CDMX",
-    latitud: 19.34692,
-    longitud: -99.1887497,
+    latitud: 19.34726165,
+    longitud: -99.1888771,
     comida: "mexicana"
   },
   {
@@ -67,9 +67,10 @@ var restaurantes = [
   },
 ];
 
+// platilla para contener propiedades de cada objeto-restaurante
 var plantillaRestaurante = '<div class="row">' +
   '<div class="col s12">' +
-      '<div class="card-panel hoverable grey lighten-5 z-depth-1">' +
+      '<div class="card-panel hoverable grey lighten-5 z-depth-1" data-latitud="__latitud__" data-longitud="__longitud__">' +
         '<h5 class="name">__nombre__</h5>' +
         '<p class="black-text">__comida__</p>' +
         '<p class="black-text">__direccion__</p>' +
@@ -77,30 +78,45 @@ var plantillaRestaurante = '<div class="row">' +
     '</div>' +
   '</div>';
 
+// filtro de restaurantes por medio de input en formulario
 var filtrarRestaurante = function (e) {
   e.preventDefault();
   var criterioBusqueda = $("#search").val().toLowerCase();
 	var restaurantesFiltrados = restaurantes.filter(function (restaurante) {
 	   return restaurante.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
 	});
-  console.log(restaurantesFiltrados);
-	 mostrarRestaurantes(restaurantesFiltrados);
+  // console.log(restaurantesFiltrados);
+	mostrarRestaurantes(restaurantesFiltrados);
 }
 
+// Imprime la lista de restaurantes reemplazando la información desde el arreglo de objeto-restaurante
 var mostrarRestaurantes = function (restaurantes) {
   console.log(restaurantes);
   var plantillaRestauranteFinal = "";
   restaurantes.forEach(function (restaurante) {
     plantillaRestauranteFinal += plantillaRestaurante.replace("__nombre__", restaurante.nombre)
     .replace("__comida__", restaurante.comida)
-    .replace("__direccion__", restaurante.direccion);
+    .replace("__direccion__", restaurante.direccion)
+    .replace("__latitud__", restaurante.latitud)
+    .replace("__longitud__", restaurante.longitud);
   });
-
+  // muestra la plantilla para cada restaurante al cargar la página
   $(".restaurante").html(plantillaRestauranteFinal);
+  // al realizar el filtro también envía al mapa correspondiente
+  $(".card-panel").click(mostrarMapaComida);
 };
 
-var mostrarMapaComida = function (restaurante) {
-  console.log(restaurantes[0].latitud);
+// Mostrar mapa del restaurante seleccionado
+function mostrarMapaComida() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+  // llama a la función que muestra la localización del restaurante y que ha localizado las coordenadas para generar el mapa correspondiente
+  initMap(coordenadas);
 }
 
 $(document).ready(cargarPagina);
